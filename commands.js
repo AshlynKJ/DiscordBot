cd85b054dbfa25fef8a9d576b08b9720e4c0422a
Ashlyn Jameson <ashlyn.k.jameson@gmail.com>
2026-08-07 17:53:05 -0400

    Inatilize files for Discord bot with simple command.

---
+++ commands.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+const HI_COMMAND = {
+  name: 'Hi',
+  description: 'Basic command',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+const ALL_COMMANDS = [HI_COMMAND];
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
